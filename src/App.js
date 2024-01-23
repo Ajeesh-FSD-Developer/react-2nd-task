302455d4188dbb0e036c914825e3903b76d4b133
Ajeesh-FSD-Developer <ajeeshk803@gmail.com>
2024-01-23 22:23:28 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
 import logo from './logo.svg';
 import './App.css';
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import Shopping from './shopping';
+import Nav from './Nav';
+import Header from './Header';
+import Footer from './Footer';
 function App() {
+
+  let Product = [
+    {
+      name: "Fancy Product",
+      price: "$40.00 - $80.00"
+    },
+    {
+      name: "Special Item",
+      price: "$20.00 $18.00"
+    },
+    {
+      name: "Sale Item",
+      price: "$50.00 $25.00"
+    },
+    {
+      name: "Popular Item",
+      price: "$40.00"
+    },
+    {
+      name: "Sale Item",
+      price: "$50.00 $25.00"
+    },
+    {
+      name: "Fancy Product",
+      price: "$120.00 - $280.00"
+    },
+    {
+      name: "Special Item",
+      price: "$20.00 $18.00"
+    },
+    {
+      name: "Popular Item",
+      price: "$40.00"
+    },
+  ]
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <Nav></Nav>
+        <Header></Header>
+        
+        <div className="row">
+
+          {
+             Product.map((Product) => {
+           return <Shopping productData={Product}></Shopping>
+           
+             })
+          }
+        </div>
+
+        <Footer></Footer>
+      </div>
+
+    </>
   );
 }
 
